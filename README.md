cdcbc729a5393de2c5b3af0870b207c9b829cc87
sleeplesseditor <josh@bryson.me.uk>
2017-11-27 01:36:57 +0000

    ReactHOCAuthentication
    A simple demonstration of authentication higher order components using React/Redux.

---
+++ README.md
@@ -0,0 +1,7 @@
+A simple demonstration of authentication higher order components using React/Redux.
+
+To get this started, follow these steps
+
+1) cd ReactHOCAuthentication
+2) npm install
+3) npm start
---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return(
+        <div>
+            Super Special Secret Recipe That Totally Is Not Made From People
+            <ul>
+                <li>1 Cup Soylent Green</li>
+                <li>1 Cup Pepper</li>
+                <li>1 Cup Salt</li>
+            </ul>
+        </div>
+    );
+};
